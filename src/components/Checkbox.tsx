99b6976e55abc7c5629382e4b4661a752894dfeb
Luiz Gustavo <gusstavopdm@gmail.com>
2023-01-20 15:28:28 -0300

    feat(Checkbox): CheckboxProps extends CheckBox.CheckboxProps

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,13 +1,13 @@
 import * as CheckBox from '@radix-ui/react-checkbox';
 import { Check } from 'phosphor-react';
 
-interface CheckboxProps {
+interface CheckboxProps extends CheckBox.CheckboxProps {
   children: string | JSX.Element | JSX.Element[];
 }
 
-export function Checkbox({ children }: CheckboxProps) {
+export function Checkbox({ children, ...rest }: CheckboxProps) {
   return (
-    <CheckBox.Root className='flex items-center gap-3 group'>
+    <CheckBox.Root className='flex items-center gap-3 group' {...rest}>
       <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500  group-data-[state=checked]:border-green-500'>
         <CheckBox.Indicator>
           <Check size={20} className='text-white' />
