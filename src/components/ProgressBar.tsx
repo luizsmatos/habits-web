a3beee8cef6d530966298b41edea7d7c93e1a9ac
Luiz Gustavo <gusstavopdm@gmail.com>
2023-01-21 11:29:06 -0300

    style(ProgressBar): add transition-all

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -11,7 +11,7 @@ export function ProgressBar(props: ProgressBarProps) {
       value={props.progress}
     >
       <Progress.Indicator
-        className='h-3 rounded-xl bg-violet-600 w-3/4'
+        className='h-3 rounded-xl bg-violet-600 w-3/4 transition-all'
         style={{ width: `${props.progress}%` }}
       />
     </Progress.Root>
