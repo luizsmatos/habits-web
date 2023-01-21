57193fca78ccacdc5f5ff41e2abecb9f3fdecef9
Luiz Gustavo <gusstavopdm@gmail.com>
2023-01-21 11:30:15 -0300

    style(NewHabitForm): add focus

--- src/components/NewHabitForm.tsx
+++ src/components/NewHabitForm.tsx
@@ -55,7 +55,7 @@ export function NewHabitForm() {
         type='text'
         id='title'
         placeholder='ex.: Exercícios, dormir bem, etc...'
-        className='p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400'
+        className='p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400 focus:outline-none focus:ring-2 focus:ring-violet-600 focus:ring-offset-2 focus:ring-offset-zinc-900'
         autoFocus
         value={title}
         onChange={(event) => setTitle(event.target.value)}
@@ -79,7 +79,7 @@ export function NewHabitForm() {
 
       <button
         type='submit'
-        className='mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500'
+        className='mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500 transition-colors focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2 focus:ring-offset-zinc-900'
       >
         <Check size={20} weight='bold' />
         Confirmar
